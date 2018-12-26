e890435aed8f9869f45be164722dfcb175f8fb4e
Maaarcocr <marcoconcettorudilosso@gmail.com>
2018-12-26 19:33:48 +0100

    using rmsync library, which now exists

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"mime/multipart"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
+	"github.com/Maaarcocr/rmsync"
 	"github.com/peterhellberg/link"
 )
 
@@ -60,70 +56,6 @@ type ZoteroDirData struct {
 	Relations        interface{} `json:"relations"`
 }
 
-type RemarkableFile struct {
-	Filename    string
-	VisibleName string
-}
-
-func getMetadataFilenames() ([]string, error) {
-	var filenames []string
-	err := filepath.Walk(BaseDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() || filepath.Ext(path) != ".metadata" {
-			return nil
-		}
-		filenames = append(filenames, path)
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return filenames, nil
-}
-
-func getDirectories(filenames []string) ([]RemarkableFile, error) {
-	var directories []RemarkableFile
-	for _, filename := range filenames {
-		filecontent, err := ioutil.ReadFile(filename)
-		if err != nil {
-			return nil, err
-		}
-		var metadata Metadata
-		err = json.Unmarshal(filecontent, &metadata)
-		if err != nil {
-			return nil, err
-		}
-		if metadata.Type == "CollectionType" {
-			directories = append(directories, RemarkableFile{filename, metadata.VisibleName})
-		}
-	}
-	return directories, nil
-}
-
-func getPdfFiles(filenames []string) ([]RemarkableFile, error) {
-	var pdfFiles []RemarkableFile
-	for _, filename := range filenames {
-		filecontent, err := ioutil.ReadFile(filename)
-		if err != nil {
-			return nil, err
-		}
-		var metadata Metadata
-		err = json.Unmarshal(filecontent, &metadata)
-		if err != nil {
-			return nil, err
-		}
-		if metadata.Type == "DocumentType" {
-			pdfFilename := strings.TrimSuffix(filename, filepath.Ext(filename)) + ".pdf"
-			if _, err := os.Stat(pdfFilename); !os.IsNotExist(err) {
-				pdfFiles = append(pdfFiles, RemarkableFile{pdfFilename, metadata.VisibleName})
-			}
-		}
-	}
-	return pdfFiles, nil
-}
-
 func getJson(url string, target interface{}) (*http.Response, error) {
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("Zotero-API-Key", ApiKey)
@@ -147,11 +79,15 @@ func getZoteroDirectories() ([]ZoteroDirectory, error) {
 
 func getZoteroItemsForDirectory(directory ZoteroDirectory) ([]ZoteroItem, error) {
 	var zoteroItems []ZoteroItem
+	fmt.Println(BaseZoteroURL + UserId + "/collections/" + directory.Key + "/items")
 	res, err := getJson(BaseZoteroURL+UserId+"/collections/"+directory.Key+"/items", &zoteroItems)
 	if err != nil {
 		return nil, err
 	}
-	next := link.ParseResponse(res)["next"].String()
+	next := ""
+	if val, ok := link.ParseResponse(res)["next"]; ok {
+		next = val.String()
+	}
 	for next != "" {
 		var tempZoteroItems []ZoteroItem
 		res, err := getJson(next, &tempZoteroItems)
@@ -178,7 +114,7 @@ func getZoteroPdfsFromItems(items []ZoteroItem) []ZoteroItem {
 	return zoteroPdfs
 }
 
-func createRemarkableFileMap(files []RemarkableFile) map[string]struct{} {
+func createRemarkableFileMap(files []rmsync.RemarkableFile) map[string]struct{} {
 	fileMap := make(map[string]struct{}, 0)
 	for _, file := range files {
 		fileMap[file.VisibleName] = struct{}{}
@@ -186,7 +122,7 @@ func createRemarkableFileMap(files []RemarkableFile) map[string]struct{} {
 	return fileMap
 }
 
-func getSharedDirectories(directories []RemarkableFile, zoteroDirectories []ZoteroDirectory) []ZoteroDirectory {
+func getSharedDirectories(directories []rmsync.RemarkableFile, zoteroDirectories []ZoteroDirectory) []ZoteroDirectory {
 	var sharedDirectories []ZoteroDirectory
 	dirMap := createRemarkableFileMap(directories)
 	for _, zoteroDirectory := range zoteroDirectories {
@@ -197,60 +133,19 @@ func getSharedDirectories(directories []RemarkableFile, zoteroDirectories []Zote
 	return sharedDirectories
 }
 
-func downloadPdfFile(url string) ([]byte, error) {
-	r, err := myClient.Get(url)
-	if err != nil {
-		return nil, err
+func createRemarkableFilesToSync(pdfs []ZoteroItem) []rmsync.FileToSync {
+	files := make([]rmsync.FileToSync, 0)
+	for _, pdf := range pdfs {
+		files = append(files, rmsync.FileToSync{pdf.Data.Filename, pdf.Data.Url})
 	}
-	defer r.Body.Close()
-	fileContents, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-	return fileContents, nil
-}
-
-func uploadPdfToTablet(fileContents []byte, filename string) error {
-	body := new(bytes.Buffer)
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("file", filename)
-	if err != nil {
-		return err
-	}
-	part.Write(fileContents)
-
-	err = writer.Close()
-	if err != nil {
-		return err
-	}
-	req, err := http.NewRequest("POST", "http://10.11.99.1/upload", body)
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("Connection", "keep-alive")
-	if err != nil {
-		return err
-	}
-	_, err = myClient.Do(req)
-	if err != nil {
-		return err
-	}
-	return nil
+	return files
 }
 
 func sync() error {
-	filenames, err := getMetadataFilenames()
+	directories, err := rmsync.GetDirectoriesMetadataFiles()
 	if err != nil {
 		return err
 	}
-	directories, err := getDirectories(filenames)
-	if err != nil {
-		return err
-	}
-	pdfFiles, err := getPdfFiles(filenames)
-	if err != nil {
-		return err
-	}
-	pdfFileMap := createRemarkableFileMap(pdfFiles)
 	zoteroDirectories, err := getZoteroDirectories()
 	if err != nil {
 		return err
@@ -262,18 +157,10 @@ func sync() error {
 			return err
 		}
 		pdfs := getZoteroPdfsFromItems(items)
-		for _, item := range pdfs {
-			if _, ok := pdfFileMap[item.Data.Filename]; !ok {
-				fmt.Println(item.Data.Url)
-				fileContents, err := downloadPdfFile(item.Data.Url)
-				if err != nil {
-					return err
-				}
-				err = uploadPdfToTablet(fileContents, item.Data.Filename)
-				if err != nil {
-					return err
-				}
-			}
+		filesToSync := createRemarkableFilesToSync(pdfs)
+		err = rmsync.Sync(filesToSync)
+		if err != nil {
+			return err
 		}
 	}
 	return nil
